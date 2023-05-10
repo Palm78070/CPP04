998847d6073e5d97dbf524c95f39fae6eb65a4b5
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2023-05-10 22:03:24 +0700

    add: make main easy to read each step

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,17 +6,31 @@
 
 int main(void)
 {
+ std::cout << "/////////Create class Animal => type Animal/////////" << std::endl;
  const Animal *meta = new Animal();
+ std::cout << "////////////////////////////////////////////////////" << std::endl;
+ std::cout << "\n/////////Create class Animal => sub-type: Dog/////////" << std::endl;
  const Animal *j = new Dog();
+ std::cout << "////////////////////////////////////////////////////" << std::endl;
+ std::cout << "\n/////////Create class Animal => sub-type: Cat/////////" << std::endl;
  const Animal *i = new Cat();
+ std::cout << "////////////////////////////////////////////////////" << std::endl;
+ std::cout << "\n/////////Get type of Dog//////////////" << std::endl;
  std::cout << j->getType() << " " << std::endl;
+ std::cout << "/////////////////////////////////////////" << std::endl;
+ std::cout << "\n/////////Get type of Cat//////////////" << std::endl;
  std::cout << i->getType() << " " << std::endl;
+ std::cout << "/////////////////////////////////////////" << std::endl;
+ std::cout << "\n////////////Make sound//////////////" << std::endl;
  i->makeSound(); // will output the cat sound!
  j->makeSound();
  meta->makeSound();
+ std::cout << "//////////////////////////////////////" << std::endl;
+ std::cout << "\n////////////Destroy Class//////////////" << std::endl;
  delete (meta);
  delete (i);
  delete (j);
+ std::cout << "//////////////////////////////////////" << std::endl;
  std::cout << "\n/////////////////Wrong Animal///////////////" << std::endl;
  const WrongAnimal *meta2 = new WrongAnimal();
  const WrongAnimal *i2 = new WrongCat();
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,12 +6,16 @@
 
 int main(void)
 {
+ std::cout << "////////Test cat class//////" << std::endl;
  AAnimal *cat = new Cat();
  cat->makeSound();
  delete cat;
+ std::cout << "///////////////////////////" << std::endl;
+ std::cout << "\n////////Test dog class//////" << std::endl;
  AAnimal *dog = new Dog();
  dog->makeSound();
  delete dog;
+ std::cout << "///////////////////////////" << std::endl;
  ///////////Test abstract class/////////////
  // AAnimal *animal = new AAnimal();
  // animal->makeSound();
